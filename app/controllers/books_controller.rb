6f6247bd1efda55c7184a0f81efc9ab787f3dec5
EC2 Default User <ec2-user@ip-172-31-41-164.ap-northeast-1.compute.internal>
2024-04-13 11:44:32 +0000

    bookers3/4/13/20:44

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,44 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+     flash[:notice] = "Book was successfully created"
+     redirect_to book_path(@book.id)
+   else
+     render :new
+   end
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_new_path
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'homes/top' => 'homes#top'
-  get  'books/new'
-  post 'books/new' => 'books#new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  
+  root to: 'homes#top'
+  get 'books/new'
+  resources :books
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
